5d8c6f4523c957d9ee5bf41a40aa5be817e04ad7
Danila <danilaphiliptsow@gmail.com>
2024-12-19 08:58:07 +0000

    initial branch commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+		// System.out.println("Hello World! It's me Kathryn!");
+		// double testVar = 3.14;
+		// System.out.println(testVar);
+		// String testStr = "Abcdefgh";
+		// System.out.println(testStr.charAt(testStr.length() - 1));
+		// try (Scanner input = new Scanner(System.in)) {
+		// 	Double testInput = input.nextDouble();
+		// 	System.out.println(testInput);
+		// 	input.close();
+		// }
+		
+		// String s = "0123456789";
+		// int maxLength = 200;
+		// String subString = s.substring(0, Math.min(maxLength, s.length()));
+		// System.out.println(subString);
+
+		// String s = "abcDe01FGhIj2";
+		// StringBuilder upperCasedChars = new StringBuilder();
+		// StringBuilder otherChars = new StringBuilder();
+		// String result;
+		// for (int i = 0; i < s.length(); i++) {
+		// 	if (Character.isUpperCase(s.charAt(i))) {
+		// 		upperCasedChars.append(s.charAt(i));
+		// 	} else {
+		// 		otherChars.append(s.charAt(i));
+		// 	}
+		// }
+		// result = upperCasedChars.toString() + otherChars.toString();
+		// System.out.println(result);
+
+		System.out.println((char) (1 + 'a'));
 	}
 }
